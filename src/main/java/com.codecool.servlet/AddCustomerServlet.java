77756bf17e03a98d9a158330c1310e899e7d271c
lpelczar <lukepelczar@gmail.com>
2018-07-04 19:15:14 +0200

    Add servlet for adding customer

---
+++ src/main/java/com.codecool.servlet/AddCustomerServlet.java
@@ -0,0 +1,18 @@
+package com.codecool.servlet;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/customer/add")
+public class AddCustomerServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("/WEB-INF/view/customer-form.jsp").forward(request,response);
+    }
+}
