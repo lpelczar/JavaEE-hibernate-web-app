ac54b624c2330035647fde591bea66284d279bf7
lpelczar <lukepelczar@gmail.com>
2018-07-06 14:29:47 +0200

    Add customer service to customer servlet

--- src/main/java/com/codecool/servlet/CustomerListServlet.java
+++ src/main/java/com/codecool/servlet/CustomerListServlet.java
@@ -1,8 +1,7 @@
 package com.codecool.servlet;
 
-import com.codecool.dao.CustomerDAO;
-import com.codecool.dao.HibernateCustomerDAO;
-import com.codecool.model.Customer;
+import com.codecool.service.CustomerService;
+import com.codecool.service.CustomerServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,18 +9,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 @WebServlet("/customer/list")
 public class CustomerListServlet extends HttpServlet {
 
+    private CustomerService customerService = new CustomerServiceImpl();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        CustomerDAO customerDAO = new HibernateCustomerDAO();
-
-        request.setAttribute("customers", customerDAO.getCustomers());
+        request.setAttribute("customers", customerService.getCustomers());
         request.getRequestDispatcher("/WEB-INF/view/list-customers.jsp").forward(request,response);
     }
 }
